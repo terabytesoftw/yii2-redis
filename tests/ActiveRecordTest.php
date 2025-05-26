b07787b6331c4f381cda4398a29e33ef3ba4a87f
Wilmer Arambula <terabytesoftw@gmail.com>
2025-05-26 17:06:56 -0400

    Replace deprecated assertContains and `assertNotContains` with `assertStringContainsString` and `assertStringNotContainsString ` in `ActiveRecordTest`.

--- tests/ActiveRecordTest.php
+++ tests/ActiveRecordTest.php
@@ -539,10 +539,10 @@ class ActiveRecordTest extends TestCase
         $script = $lua->buildOne($query);
 
         foreach($expectedStrings as $string) {
-            $this->assertContains($string, $script);
+            $this->assertStringContainsString($string, $script);
         }
         foreach($unexpectedStrings as $string) {
-            $this->assertNotContains($string, $script);
+            $this->assertStringNotContainsString($string, $script);
         }
     }
 
